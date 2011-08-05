8c975247296032df1eb3838ad6c622a7cd971c35
Raymond Cohen <raycohen@gmail.com>
2011-08-04 22:53:25 -0400

    progress

--- js/filter.js
+++ js/filter.js
@@ -1,56 +1,11 @@
 /*
- * Jeter Filter - Content Script
+ * Comment Filter - Content Script
  * 
- * This is the primary JS file that manages the detection and filtration of Number Two from the web page.
+ * This is the primary JS file that manages the detection and filtration of Comments from the web page.
  */
+console.info('removing Disqus element');
+$("#disqus_thread").remove();
+$("#comments").remove();
 
-// Variables
-var regex = /Jeter/i;
-var search = regex.exec(document.body.innerText);
-
-
-// Functions
-function filterMild() {
-	console.log("Filtering Number Two with Vindictive filter...");
-	return $(":contains('Jeter'), :contains('JETER'), :contains('jeter')").filter(":not('body'):not('html'):not('div')");
-}
-
-function filterDefault () {
-	console.log("Filtering Number Two with Default filter...");
-	return $(":contains('Jeter'), :contains('JETER'), :contains('jeter')").filter(":not('body'):not('html'):not('div')").closest('div');
-}
-
-function filterVindictive() {
-	console.log("Filtering Number Two with Vindictive filter...");
-	return $(":contains('Jeter'), :contains('JETER'), :contains('jeter')").filter(":only-child:last-child:not('body'):not('html')");
-}
-
-function getElements(filter) {
-   if (filter == "Mild") {
-	   return filterMild();
-   } else if (filter == "Vindictive") {
-	   return filterVindictive();
-   } else {
-	   return filterDefault();
-   }
-}
-
-function filterElements(elements) {
-	console.log("Elements to filter: ", elements);
-	elements.fadeOut("fast");
-}
-
-
-// Implementation
-if (search) {
-   console.log("Number Two found on page! - Searching for elements...");
-   chrome.extension.sendRequest({method: "loadFilter"}, function(response) {
-	   console.log("Filter setting stored is: " + response.filter);
-	   elements = getElements(response.filter);
-	   filterElements(elements);
-	   chrome.extension.sendRequest({method: "saveStats", jeters: elements.length}, function(response) {
-			  console.log("Logging " + elements.length + " jeters."); 
-		   });
-	 });
-  chrome.extension.sendRequest({}, function(response) {});
-}
+$("iframe[src^=http://www.facebook.com/plugins/comments.php]").remove();
+$("iframe[src^=https://www.facebook.com/plugins/comments.php]").remove();
