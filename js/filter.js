e5d26779adca04ab695e989456f52d922fdd5cdf
Raymond Cohen <raycohen@gmail.com>
2011-08-05 06:26:09 -0400

    remove unused files, better facebook comments removal

--- js/filter.js
+++ js/filter.js
@@ -3,16 +3,18 @@
  * 
  * This is the primary JS file that manages the detection and filtration of Comments from the web page.
  */
-console.info('removing Disqus element');
 
 function filterComments() {
+  console.info('cleansing Disqus');
   $("#disqus_thread").remove();
   $("#comments").remove();
 
   $('iframe.comments').remove();
 
+  console.info('cleansing Facebook');
   $('iframe[src^="http://www.facebook.com/plugins/comments.php"]').remove();
-  $('iframe[src^="https://www.facebook.com/plugins/comments.php"]').remove();
+  $('iframe[src^="https://www.facebook.com/plugins/comments.php"]').remove();  
+  $(document.getElementsByTagName('fb:comments')).remove();
   
   $('script[src^="http://intensedebate.com/comment"]').parent().remove();
   
