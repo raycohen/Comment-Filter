3e2660582989f33e0859cc8c317167a76c307745
Raymond Cohen <raycohen@gmail.com>
2011-08-04 23:05:30 -0400

    correctly remove Facebook comments modules, also remove ones that are added to the page a little later.

--- js/filter.js
+++ js/filter.js
@@ -4,8 +4,17 @@
  * This is the primary JS file that manages the detection and filtration of Comments from the web page.
  */
 console.info('removing Disqus element');
-$("#disqus_thread").remove();
-$("#comments").remove();
 
-$("iframe[src^=http://www.facebook.com/plugins/comments.php]").remove();
-$("iframe[src^=https://www.facebook.com/plugins/comments.php]").remove();
+function filterComments() {
+  $("#disqus_thread").remove();
+  $("#comments").remove();
+
+  $('iframe.comments').remove();
+
+  $('iframe[src^="http://www.facebook.com/plugins/comments.php"]').remove();
+  $('iframe[src^="https://www.facebook.com/plugins/comments.php"]').remove();  
+}
+
+filterComments();
+
+setTimeout(filterComments, 5000);
